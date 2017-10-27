b682b7d5a4c22448cafa3330f0c2732c1eb2c485
Samyak Singh <samyasin@visa.com>
2017-10-27 16:36:21 -0700

    Empty directories are generated by the script instead of being copied

--- init.sh
+++ init.sh
@@ -5,20 +5,49 @@ if [ -z "$1" ]
 		echo "Error: Please provide an app name argument."
 fi
 
+echo Creating your app...
+
 appDir=$1
 
 mkdir $appDir
 
 # app meta creation
-cp dev/meta/* $appDir/
+cp meta/* $appDir/
 
 # src dir creation
-cp -R dev/src $appDir/
+mkdir $appDir/src
+
+mkdir $appDir/src/js
+
+mkdir $appDir/src/js/actions
+mkdir $appDir/src/js/components
+mkdir $appDir/src/js/containers
+mkdir $appDir/src/js/middlewares
+mkdir $appDir/src/js/reducers
+mkdir $appDir/src/js/routes
+mkdir $appDir/src/js/utilities
+
+mkdir $appDir/src/scss
+
 mkdir $appDir/src/img
+
 mkdir $appDir/src/content
 
+touch $appDir/src/js/actions/types.js
+touch $appDir/src/js/actions/index.js
+touch $appDir/src/js/middlewares/index.js
+touch $appDir/src/js/reducers/index.js
+touch $appDir/src/scss/styles.scss
+
 # test dir creation
-cp -R dev/test $appDir/
+mkdir $appDir/test/
+mkdir $appDir/test/actions
+mkdir $appDir/test/components
+mkdir $appDir/test/containers
+mkdir $appDir/test/middlewares
+mkdir $appDir/test/reducers
+mkdir $appDir/test/routes
+mkdir $appDir/test/utilities
 
 # dist dir creation
 cp -R dev/dist $appDir/
@@ -29,4 +58,7 @@ cp configs/.* $appDir/
 
 # install modules
 cd $appDir
+
+echo Installing node modules...
+
 npm install
